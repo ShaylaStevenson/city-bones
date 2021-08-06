0d140f90b60733ae834bd8fc74484f869b3683f3
Shayla Stevenson <74476502+Shazzyshay@users.noreply.github.com>
2021-08-06 13:48:51 -0700

    Updates

---
+++ precache-manifest.6c217effec4af0087d6a93b545212311.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d42e65f7b35a1c0f5cec",
+    "url": "/city-bones/static/css/main.a3bbd124.chunk.css"
+  },
+  {
+    "revision": "d42e65f7b35a1c0f5cec",
+    "url": "/city-bones/static/js/main.c9655132.chunk.js"
+  },
+  {
+    "revision": "eebb65937b912caf10c9",
+    "url": "/city-bones/static/js/runtime~main.a3f53dca.js"
+  },
+  {
+    "revision": "e3a68598d774fc570f7a",
+    "url": "/city-bones/static/js/2.32630e8e.chunk.js"
+  },
+  {
+    "revision": "081a2087003eb1c3b32b4885f1753486",
+    "url": "/city-bones/static/media/wood-kitsap-overlay-1280x500.081a2087.jpg"
+  },
+  {
+    "revision": "d050e29c20b9b0179d95233bff4d66b9",
+    "url": "/city-bones/index.html"
+  }
+];
